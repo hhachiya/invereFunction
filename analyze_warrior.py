12c362aaeecdad95d477d9d128e721da7ccbd257
Hirotaka Hachiya <hacchan@gmail.com>
2019-01-10 22:24:13 +0900

    change file name from visualize_warrior to embedding_warrior

---
+++ analyze_warrior.py
@@ -0,0 +1,20 @@
+import pdb
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import pickle as pkl
+import os
+import matplotlib.pylab as plt
+from sklearn.cluster import KMeans
+
+clusterOutPath = 'warrior-data-cluster/analysis.pickle'
+
+with open(clusterOutPath,'rb') as fp:
+	src_data = pkl.load(fp)
+	tar_data = pkl.load(fp)
+	top_similar_words = pkl.load(fp)
+	kmeans = pkl.load(fp)
+	warrior_names = pkl.load(fp)
+	warrior_names_by_cluster = pkl.load(fp)
+	fig = pkl.load(fp)
+	
