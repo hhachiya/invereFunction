4f51cb564ff3a165e053515665eab54d9f2972d2
Hirotaka Hachiya <hhachiya@sys.wakayama-u.ac.jp>
2019-01-10 23:32:01 +0900

    update

--- analyze_warrior.py
+++ analyze_warrior.py
@@ -16,9 +16,21 @@ with open(clusterOutPath,'rb') as fp:
 	kmeans = pkl.load(fp)
 	warrior_names = pkl.load(fp)
 	warrior_names_by_cluster = pkl.load(fp)
-	pdb.set_trace()
 	fig = pkl.load(fp)
-	
-fig.show()
 
-pdb.set_trace()
+# plot image	
+fig.show()
+plt.savefig('analysis_cluster.png')
+
+# top 5 words for each cluster
+input('Enterを押すと, 各クラスター中心に関連する語top5を表示します:')
+for c in range(len(top_similar_words)):
+	print("クラスター{}: {}".format(c,top_similar_words[c][:5]))
+print("\n")
+
+for c in range(len(warrior_names_by_cluster)):
+	input('Enterを押すと, クラスター{}に属する武将一覧を表示します:'.format(c))
+	print(warrior_names_by_cluster[c])
+	print("\n")
+
+input('Enterを押すと, 終了します')
